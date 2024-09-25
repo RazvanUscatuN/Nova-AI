44601f8c93026e497588e17354fcb7e197846f84
Razvan U <129730664+RazvanUscatuN@users.noreply.github.com>
2024-09-25 13:13:30 +0300

    setting comments

--- main.py
+++ main.py
@@ -1,1 +1,45 @@
-print("hello world")
+""" 
+Creating Nova, ai assitant that focus on being a personal assistant or teacher (for the moment)
+The main logic for this AI: capture sound from mic, converting into text, process the text and respond trough LLM,
+voice the respond
+Factors that I am looking for: speed, accurate repsonses, natural voice
+Features(maybe future): memory, pre-load info,  setting reminders
+Others: using open sources ideas, runnig llm locally as a test.
+This project is focusing on learning, testing and breaking stuff until it works 
+
+"""
+
+# 3 main logic pillars
+
+# I. Capture 
+# II. Process
+# III. Deliver
+
+
+# I. Capture
+# Focus on capturing the sound from the mic, converting into text  with whisper
+
+
+
+
+
+
+
+
+
+# II. Process
+# The text response is feed into a local LLM and produce a respond in a text back
+
+
+
+
+
+
+
+
+
+
+# III. Deliver
+# The response from the LLM is voices through a AI voice. Focus is on natural voice as much as posible.
+# Try with open source, if not the big tech ai voices altenative
+ 
